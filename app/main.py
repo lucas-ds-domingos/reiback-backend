a6e58963e09ef0bf5d34b466d199ef1a759403fc
lucas-ds-domingos <lucasdomingos.ds@hotmail.com>
2025-10-01 16:27:10 -0300

    altt de main

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://reiback-front-production.up.railway.app/",  # seu front deployado
-        "http://localhost:3000",  # localhost para teste
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
