e26a3c49a1a71783492e4e85dc6f16ac7ad547f7
lucas-ds-domingos <lucasdomingos.ds@hotmail.com>
2025-10-08 14:44:33 -0300

    criação de rota do representante legal 1

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 from .database import Base
 from datetime import datetime
 from decimal import Decimal
+from sqlalchemy.sql import func
 
 
 class Usuario(Base):
