f2847a0683b15bb65a0201181557c0ad31f3a7ab
lucas-ds-domingos <lucasdomingos.ds@hotmail.com>
2025-10-30 10:27:17 -0300

    att no envio de email 2

--- app/utils/email.py
+++ app/utils/email.py
@@ -8,9 +8,9 @@ import ssl
 load_dotenv()
 
 def enviar_email(para: str, assunto: str, corpo: str):
-    host = os.getenv("EMAIL_HOST")  # mail.financeassurance.com.br
-    port = int(os.getenv("EMAIL_PORT"))  # 587
-    user = os.getenv("EMAIL_USER")  # finance@financeassurance.com.br
+    host = os.getenv("EMAIL_HOST") 
+    port = int(os.getenv("EMAIL_PORT"))  
+    user = os.getenv("EMAIL_USER") 
     password = os.getenv("EMAIL_PASS")
     remetente = os.getenv("EMAIL_FROM")
 
@@ -21,11 +21,10 @@ def enviar_email(para: str, assunto: str, corpo: str):
     msg.attach(MIMEText(corpo, "html"))
 
     try:
-        # Cria contexto TLS seguro
         context = ssl.create_default_context()
 
         with smtplib.SMTP(host, port) as server:
-            server.starttls(context=context)  # STARTTLS na porta 587
+            server.starttls(context=context) 
             server.login(user, password)
             server.send_message(msg)
 
